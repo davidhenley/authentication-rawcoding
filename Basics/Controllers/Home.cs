eebecd6b600fcccfc80867372ab473200697961a
David Henley <henleydr@gmail.com>
2021-01-08 14:42:27 -0600

    Basics

---
+++ Basics/Controllers/Home.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            // Create claims and user
+            var grandmasClaims = new List<Claim>
+            {
+                new(ClaimTypes.Name, "Bob"),
+                new(ClaimTypes.Email, "bob@example.com"),
+                new("Grandma.Says", "Very nice boy")
+            };
+            
+            var licenseClaims = new List<Claim>
+            {
+                new(ClaimTypes.Name, "Bob K Foo"),
+                new("DriverLicense", "A+")
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmasClaims, "Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government");
+
+            var userPrincipal = new ClaimsPrincipal(new[] {grandmaIdentity, licenseIdentity});
+
+            // Sign in user (set cookie)
+            HttpContext.SignInAsync(userPrincipal);
+            
+            // Redirect
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Basics/Startup.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Basics
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth", config =>
+                {
+                    config.Cookie.Name = "Grandmas.Cookie";
+                    config.LoginPath = "/Home/Authenticate";
+;                });
+            
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseRouting();
+            
+            // Authentication must go between routing and endpoints
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
